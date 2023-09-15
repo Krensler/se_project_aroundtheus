bea3e7cb337bbd2a644838d284fba7e815eb335f
Kai Krensler <kaikrensler@gmail.com>
2023-09-15 14:07:05 -0400

    Full commit, learning to be better about commiting thoughout!

--- scripts/index.js
+++ scripts/index.js
@@ -76,10 +76,19 @@ const modalTextEl = document.querySelector("#modal-text");
 
 function openModal(modal) {
   modal.classList.add("modal_opened");
+  document.addEventListener("keydown", closeWithEscape);
 }
 
 function closeModal(modal) {
   modal.classList.remove("modal_opened");
+  document.removeEventListener("keydown", closeWithEscape);
+}
+
+function closeWithEscape(evt) {
+  if (evt.key === "Escape") {
+    const openModal = document.querySelector(".modal_opened");
+    closeModal(openModal);
+  }
 }
 
 function getCardElement(cardData) {
---
+++ scripts/validation.js
@@ -0,0 +1,81 @@
+function showInputError(formElement, inputElement, options) {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.add(options.inputErrorClass);
+  errorMessageElement.textContent = inputElement.validationMessage;
+  errorMessageElement.classList.add(options.errorClass);
+}
+
+function hideInputError(formElement, inputElement, options) {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.remove(options.inputErrorClass);
+  errorMessageElement.textContent.reset;
+  errorMessageElement.classList.remove(options.errorClass);
+}
+
+function checkInputValidity(formElement, inputElement, options) {
+  if (!inputElement.validity.valid) {
+    return showInputError(formElement, inputElement, options);
+  }
+  hideInputError(formElement, inputElement, options);
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputElement) => inputElement.validity.valid);
+}
+
+function disableButton(submitButton, options) {
+  submitButton.classList.add(options.inactiveButtonClass);
+  submitButton.disabled = true;
+}
+
+function enableButton(submitButton, options) {
+  submitButton.classList.remove(options.inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function toggleButtonState(inputElements, submitButton, options) {
+  if (hasInvalidInput(inputElements)) {
+    return disableButton(submitButton, options);
+  }
+  enableButton(submitButton, options);
+}
+
+function setEventListeners(formElement, options) {
+  const { inputSelector } = options;
+  const inputElements = [
+    ...formElement.querySelectorAll(options.inputSelector),
+  ];
+  const submitButton = formElement.querySelector(options.submitButtonSelector);
+  inputElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, options);
+      toggleButtonState(inputElements, submitButton, options);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, options);
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__form-input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__form-input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
