6c460dbaaba07e1f593ee6bcd30f2be1600be642
Kai Krensler <kaikrensler@gmail.com>
2023-11-14 12:03:00 -0500

    fixed modals

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 export default class Card {
-  constructor({ data }, cardSelector, handleImageClick) {
+  constructor(data, cardSelector, handleImageClick) {
     this._name = data.name;
     this._link = data.link;
     this._cardSelector = cardSelector;
